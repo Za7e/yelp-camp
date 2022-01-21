b05ce2b38cf0a0f68df087b253f130b55b71502b
Zale Davis <zale.davis@gmx.com>
2022-01-21 12:52:49 -0800

    add images and description to seed db

--- seeds/index.js
+++ seeds/index.js
@@ -20,9 +20,13 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20 + 10);
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/10489597',
+            description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas reprehenderit aut eum porro dignissimos animi similique ea, inventore fugit, ipsa beatae, nam cum id nihil delectus nobis amet exercitationem repellat!',
+            price
         })
         await camp.save();
     }
