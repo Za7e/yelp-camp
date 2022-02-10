0a797ef877ba72d285a9a6865b585444574e720e
Zale Davis <zale.davis@gmx.com>
2022-02-10 10:45:05 -0800

    add helmet for security and set up mongo store

--- app.js
+++ app.js
@@ -14,14 +14,17 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user');
 const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
 
 const userRoutes = require('./routes/users');
 const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
 
+const MongoStore = require('connect-mongo')
 
+const dbUrl = 'mongodb://localhost:27017/yelp-camp'
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+mongoose.connect(dbUrl, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
@@ -45,7 +48,21 @@ app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(mongoSanitize());
 
+const store = MongoStore.create({
+    mongoUrl: dbUrl,
+    crypto: {
+        secret: 'thisshouldbebetter'
+    },
+    touchAfter: 24 * 60 * 60
+});
+
+store.on("error", function (e) {
+    console.log("SESSION STORE ERROR", e)
+})
+
 const sessionConfig = {
+    store,
+    name: 'session',
     secret: 'thisshouldbebetter',
     resave: false,
     saveUninitialized: true,
@@ -57,6 +74,64 @@ const sessionConfig = {
 }
 app.use(session(sessionConfig));
 app.use(flash());
+
+
+// app.use(helmet({ crossOriginEmbedderPolicy: false }));
+
+const scriptSrcUrls = [
+    "https://stackpath.bootstrapcdn.com/",
+    "https://api.tiles.mapbox.com",
+    "https://api.mapbox.com/",
+    "https://kit.fontawesome.com/",
+    "https://cdnjs.cloudflare.com/",
+    "https://cdn.jsdelivr.net/",
+    "https://res.cloudinary.com/za7e/",
+    "https://images/unsplash.com/"
+];
+const styleSrcUrls = [
+    "https://kit-free.font-awesome.com/",
+    "https://stackpath.bootstrapcdn.com/",
+    "https://api.mapbox.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://fonts.googleapis.com/",
+    "https://use.fontawesome.com/",
+    "https://cdn.jsdelivr.net/",
+    "https://res.cloudinary.com/za7e/",
+    "https://images/unsplash.com/"
+];
+const connectSrcUrls = [
+    "https://*.tiles.mapbox.com/",
+    "https://api.mapbox.com/",
+    "https://events.mapbox.com/",
+    "https://res.cloudinary.com/za7e/",
+    "https://images/unsplash.com/"
+];
+const fontSrcUrls = ["https://res.cloudinary.com/za7e/"];
+app.use(
+    helmet({
+        contentSecurityPolicy: {
+            directives: {
+                defaultSrc: [],
+                connectSrc: ["'self'", ...connectSrcUrls],
+                scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+                styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+                workerSrc: ["'self'", "blob:"],
+                objectSrc: [],
+                imgSrc: [
+                    "'self'",
+                    "blob:",
+                    "data:",
+                    "https://res.cloudinary.com/za7e/",
+                    "https://images.unsplash.com/",
+                ],
+                fontSrc: ["'self'", ...fontSrcUrls],
+                mediaSrc: ["https://res.cloudinary.com/dv5vm4sqh/"],
+                childSrc: ["blob:"]
+            },
+        },
+        crossOriginEmbedderPolicy: false
+    })
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
